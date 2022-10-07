0283dab1d130591504928169e644a3de9f11cf36
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-07 18:42:26 +0000

    Adding tarfile member sanitization to extractall()

--- content/Easy-ELECTRA/electrax/data/pretrain_dataset.py
+++ content/Easy-ELECTRA/electrax/data/pretrain_dataset.py
@@ -199,7 +199,29 @@ class TFPretrainExamplesGenerator():
         job_tmp_dir = os.path.join(self.config.output_dir, "tmp", "job_" + str(self.job_id))
         tfutils.rmkdir(job_tmp_dir)
         with tarfile.open(os.path.join(self.config.corpus_dir, fname)) as f:
-            f.extractall(job_tmp_dir)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, job_tmp_dir)
         extracted_files = tf.io.gfile.listdir(job_tmp_dir)
         random.shuffle(extracted_files)
         return extracted_files
